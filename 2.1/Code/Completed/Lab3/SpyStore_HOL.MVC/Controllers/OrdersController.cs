6f3e632c0e1543c0c5b489b1872afd20aff88467
Philip Japikse <skimedic@gmail.com>
2018-09-03 22:52:17 -0400

    added per lab breakdowns of the code

---
+++ 2.1/Code/Completed/Lab3/SpyStore_HOL.MVC/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpyStore_HOL.DAL.Repos.Interfaces;
+
+namespace SpyStore_HOL.MVC.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrderRepo _orderRepo;
+        public OrdersController(IOrderRepo orderRepo)
+        {
+            _orderRepo = orderRepo;
+        }
+
+    }
+}
---
+++ 2.1/Code/Completed/Lab7/SpyStore_HOL.MVC/ViewModels/CartRecordViewModel.cs
@@ -0,0 +1,9 @@
+﻿using SpyStore_HOL.MVC.ViewModels.Base;
+
+namespace SpyStore_HOL.MVC.ViewModels
+{
+    public class CartRecordViewModel : CartViewModelBase
+    {
+        public int Quantity { get; set; }
+    }
+}
