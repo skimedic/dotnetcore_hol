059bcebe3eafad06ece115ce6eb4d75486075ff3
Philip Japikse <skimedic@gmail.com>
2019-02-25 01:38:34 -0500

    updating mvc for 2.2

---
+++ 2.2/Code/Completed/LabMvc/SpyStore.Hol.Mvc/Models/ViewModels/CartRecordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using SpyStore.Hol.Models.ViewModels;
+using SpyStore.Hol.Mvc.Validation;
+
+namespace SpyStore.Hol.Mvc.Models.ViewModels
+{
+    public class CartRecordViewModel : CartRecordWithProductInfo
+    {
+        [Required,MustNotBeGreaterThan(nameof(UnitsInStock))]
+        public new int Quantity { get; set; }
+    }
+}
