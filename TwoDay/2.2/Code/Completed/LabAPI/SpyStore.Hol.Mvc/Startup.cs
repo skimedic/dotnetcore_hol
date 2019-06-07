dbafb3b439ce5d77a8b52496c3a21377d7cccfa0
Philip Japikse <skimedic@gmail.com>
2019-06-07 07:20:50 -0400

    updating MVC site to use service

--- TwoDay/2.2/Code/Completed/LabAPI/SpyStore.Hol.Mvc/Startup.cs
+++ TwoDay/2.2/Code/Completed/LabAPI/SpyStore.Hol.Mvc/Startup.cs
@@ -6,15 +6,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SpyStore.Hol.Mvc.Support;
 
 namespace SpyStore.Hol.Mvc
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IHostingEnvironment _env;
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
+            _env = env;
             Configuration = configuration;
         }
 
@@ -23,21 +28,47 @@ namespace SpyStore.Hol.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.Configure<ServiceSettings>(Configuration.GetSection("ServiceSettings"));
+            services.AddHttpClient<SpyStoreServiceWrapper>();
+            if (_env.IsDevelopment() || _env.EnvironmentName == "Local")
+            {
+                //services.AddWebOptimizer(false,false);
+                services.AddWebOptimizer(options =>
+                {
+                    options.MinifyCssFiles(); //Minifies all CSS files
+                    //options.MinifyJsFiles(); //Minifies all JS files
+                    options.MinifyJsFiles("js/site.js");
+                    options.AddJavaScriptBundle("js/validations/validationCode.js", "js/validations/**/*.js");
+                    //options.AddJavaScriptBundle("js/validations/validationCode.js", "js/validations/validators.js", "js/validations/errorFormatting.js");
+                });
+
+            }
+            else
+            {
+                services.AddWebOptimizer(options =>
+                {
+                    options.MinifyCssFiles(); //Minifies all CSS files
+                    //options.MinifyJsFiles(); //Minifies all JS files
+                    options.MinifyJsFiles("js/site.js");
+                    options.AddJavaScriptBundle("js/validations/validationCode.js", "js/validations/**/*.js");
+                    //options.AddJavaScriptBundle("js/validations/validationCode.js", "js/validations/validators.js", "js/validations/errorFormatting.js");
+                });
+            }
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Local"))
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -46,14 +77,17 @@ namespace SpyStore.Hol.Mvc
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseWebOptimizer();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            //app.UseCookiePolicy();
 
+            //app.UseMvcWithDefaultRoute();
+            //app.UseMvc();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
---
+++ TwoDay/2.2/Code/Completed/LabAPI/SpyStore.Hol.Mvc/Support/SpyStoreServiceWrapper.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using SpyStore.Hol.Models.Entities;
+using SpyStore.Hol.Models.ViewModels;
+
+namespace SpyStore.Hol.Mvc.Support
+{
+    public interface ISpyStoreServiceWrapper
+    {
+        //CategoryController
+        Task<IList<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryAsync(int id);
+        Task<IList<Product>> GetProductsForACategoryAsync(int categoryId);
+        ////Orders Controller
+        //Task<IList<Order>> GetOrdersAsync(int customerId);
+        //Task<OrderWithDetailsAndProductInfo> GetOrderDetailsAsync(int orderId);
+        //Product Controller
+        Task<Product> GetOneProductAsync(int productId);
+        Task<IList<Product>> GetFeaturedProductsAsync();
+        //Search Controller
+        Task<IList<Product>> SearchAsync(string searchTerm);
+        //Shopping Cart Record Controller
+        Task<IList<CartRecordWithProductInfo>> GetCartRecordsAsync(int id);
+        //Task<string> AddToCartAsync(int customerId, int productId, int quantity);
+        //Task<string> UpdateCartItemAsync(ShoppingCartRecord item);
+        //Task RemoveCartItemAsync(ShoppingCartRecord item);
+        //Shopping Cart Controller
+        Task<CartWithCustomerInfo> GetCartAsync(int customerId);
+        Task<string> PurchaseAsync(int customerId, Customer customer);
+        //Customer Controller
+        Task<Customer> GetCustomerAsync(int customerId);
+        Task<IList<Customer>> GetCustomersAsync();
+    }
+
+    public class SpyStoreServiceWrapper : ISpyStoreServiceWrapper
+    {
+        private HttpClient _client;
+        private ServiceSettings _settings;
+
+        public SpyStoreServiceWrapper(HttpClient client, IOptionsSnapshot<ServiceSettings> settings)
+        {
+            _client = client;
+            _settings = settings.Value;
+            _client.BaseAddress = new Uri(_settings.Uri);
+        }
+
+        public async Task<IList<Category>> GetCategoriesAsync()
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.CategoryBaseUri}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<IList<Category>>();
+
+            return result;
+        }
+
+        public async Task<Category> GetCategoryAsync(int id)
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.CategoryBaseUri}/{id}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<Category>();
+
+            return result;
+        }
+
+        public async Task<IList<Product>> GetProductsForACategoryAsync(int categoryId)
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.CategoryBaseUri}/{categoryId}/products");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<IList<Product>>();
+
+            return result;
+        }
+
+        public async Task<Product> GetOneProductAsync(int productId)
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.ProductBaseUri}/{productId}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<Product>();
+
+            return result;
+        }
+
+        public async Task<IList<Product>> GetFeaturedProductsAsync()
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.ProductBaseUri}/featured");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<IList<Product>>();
+
+            return result;
+        }
+
+        public async Task<IList<Product>> SearchAsync(string searchTerm)
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.SearchBaseUri}/{searchTerm}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<IList<Product>>();
+
+            return result;
+        }
+
+        public async Task<IList<CartRecordWithProductInfo>> GetCartRecordsAsync(int id)
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.CartRecordBaseUri}/{id}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<IList<CartRecordWithProductInfo>>();
+
+            return result;
+        }
+
+        public async Task<CartWithCustomerInfo> GetCartAsync(int customerId)
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.CartBaseUri}/{customerId}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<CartWithCustomerInfo>();
+
+            return result;
+        }
+
+        public async Task<string> PurchaseAsync(int customerId, Customer customer)
+        {
+            var response = await _client.PostAsJsonAsync($"{_settings.Uri}{_settings.CartBaseUri}/{customerId}",customer);
+
+            response.EnsureSuccessStatusCode();
+            
+            var result = await response.Content.ReadAsAsync<Product>();
+
+            return null;
+        }
+
+        public async Task<Customer> GetCustomerAsync(int customerId)
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.CustomerBaseUri}/{customerId}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<Customer>();
+
+            return null;
+        }
+
+        public async Task<IList<Customer>> GetCustomersAsync()
+        {
+            var response = await _client.GetAsync($"{_settings.Uri}{_settings.CustomerBaseUri}");
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsAsync<IList<Customer>>();
+
+            return result;
+
+        }
+    }
+}
