930404edc3e4da85fa09fb7573640ede23154ac4
Philip Japikse <skimedic@gmail.com>
2019-08-16 23:58:12 -0400

    updated labs 6 and 7

--- SingleDayHandsOnLabFiles/2.2/Code/Completed/Lab6/SpyStore.Hol.Mvc/Startup.cs
+++ SingleDayHandsOnLabFiles/2.2/Code/Completed/Lab6/SpyStore.Hol.Mvc/Startup.cs
@@ -32,12 +32,12 @@ namespace SpyStore.Hol.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.Configure<CookiePolicyOptions>(options =>
-            //{
-            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-            //    options.CheckConsentNeeded = context => true;
-            //    options.MinimumSameSitePolicy = SameSiteMode.None;
-            //});
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             var connectionString = Configuration.GetConnectionString("SpyStore");
@@ -82,7 +82,7 @@ namespace SpyStore.Hol.Mvc
             }
 
             app.UseStaticFiles();
-            //app.UseCookiePolicy();
+            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
