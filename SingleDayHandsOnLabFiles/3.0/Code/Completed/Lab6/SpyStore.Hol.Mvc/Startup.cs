644d3817cf7c6d33ce5f1075e8e904dea27fb629
skimedic <skimedic@gmail.com>
2019-10-04 17:04:28 -0400

    finished 3.0 version of the HOL

--- SingleDayHandsOnLabFiles/3.0/Code/Completed/Lab6/SpyStore.Hol.Mvc/Startup.cs
+++ SingleDayHandsOnLabFiles/3.0/Code/Completed/Lab6/SpyStore.Hol.Mvc/Startup.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SpyStore.Hol.Dal.EfStructures;
+using SpyStore.Hol.Dal.Initialization;
+using SpyStore.Hol.Dal.Repos;
+using SpyStore.Hol.Dal.Repos.Interfaces;
+using SpyStore.Hol.Mvc.Support;
 
 namespace SpyStore.Hol.Mvc
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            _env = env;
             Configuration = configuration;
         }
 
@@ -23,19 +31,36 @@ namespace SpyStore.Hol.Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            var connectionString = Configuration.GetConnectionString("SpyStore");
+            services.AddDbContextPool<StoreContext>(options => options
+                .UseSqlServer(connectionString,o=>o.EnableRetryOnFailure()));
+            services.AddScoped<ICategoryRepo, CategoryRepo>();
+            services.AddScoped<IProductRepo, ProductRepo>();
+            services.AddScoped<ICustomerRepo, CustomerRepo>();
+            services.AddScoped<IShoppingCartRepo, ShoppingCartRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddScoped<IOrderDetailRepo, OrderDetailRepo>();
+            services.Configure<CustomSettings>(Configuration.GetSection("CustomSettings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Local"))
             {
                 app.UseDeveloperExceptionPage();
+                using (var serviceScope = app.ApplicationServices
+                    .GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    SampleDataInitializer.InitializeData(
+                        serviceScope.ServiceProvider.GetRequiredService<StoreContext>());
+                }
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
             app.UseStaticFiles();
 
             app.UseRouting();
