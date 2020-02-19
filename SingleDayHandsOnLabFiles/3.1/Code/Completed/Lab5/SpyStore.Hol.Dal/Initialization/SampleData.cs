4fa4bca0712d19a1a1d120a7818b2db90161c845
skimedic <skimedic@gmail.com>
2020-02-19 01:08:07 -0500

    one day updates

--- SingleDayHandsOnLabFiles/3.1/Code/Completed/Lab5/SpyStore.Hol.Dal/Initialization/SampleData.cs
+++ SingleDayHandsOnLabFiles/3.1/Code/Completed/Lab5/SpyStore.Hol.Dal/Initialization/SampleData.cs
@@ -1,16 +1,4 @@
-﻿#region copyright
-
-// Copyright Information
-// ==================================
-// SpyStore.Hol - SpyStore.Hol.Dal - SampleData.cs
-// All samples copyright Philip Japikse
-// http://www.skimedic.com 2019/10/04
-// See License.txt for more information
-// ==================================
-
-#endregion
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using SpyStore.Hol.Models.Entities;
 using SpyStore.Hol.Models.Entities.Base;
--- SingleDayHandsOnLabFiles/3.1/Code/Completed/Lab5/SpyStore.Hol.Dal/Initialization/SampleDataInitializer.cs
+++ SingleDayHandsOnLabFiles/3.1/Code/Completed/Lab5/SpyStore.Hol.Dal/Initialization/SampleDataInitializer.cs
@@ -1,16 +1,4 @@
-﻿#region copyright
-
-// Copyright Information
-// ==================================
-// SpyStore.Hol - SpyStore.Hol.Dal - SampleDataInitializer.cs
-// All samples copyright Philip Japikse
-// http://www.skimedic.com 2019/10/04
-// See License.txt for more information
-// ==================================
-
-#endregion
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
