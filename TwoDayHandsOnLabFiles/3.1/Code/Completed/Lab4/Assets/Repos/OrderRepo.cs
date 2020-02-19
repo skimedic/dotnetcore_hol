164d34c5e536b5df2725ac6fd87c8c9ea9ce4066
skimedic <skimedic@gmail.com>
2020-02-19 01:10:50 -0500

    labs 2-5

---
+++ TwoDayHandsOnLabFiles/3.1/Code/Completed/Lab4/Assets/Repos/OrderRepo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SpyStore.Hol.Dal.EfStructures;
+using SpyStore.Hol.Dal.Repos.Base;
+using SpyStore.Hol.Dal.Repos.Interfaces;
+using SpyStore.Hol.Models.Entities;
+using SpyStore.Hol.Models.ViewModels;
+
+namespace SpyStore.Hol.Dal.Repos
+{
+    public class OrderRepo : RepoBase<Order>,IOrderRepo
+    {
+        private bool _isDisposed;
+        public OrderRepo(
+            StoreContext context) : base(context)
+        {
+        }
+        internal OrderRepo(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (_isDisposed)
+            {
+                return;
+            } 
+      
+            if (disposing) {
+            }
+      
+            _isDisposed = true;
+            // Call base class implementation.
+            base.Dispose(disposing);
+        }
+    }
+}
