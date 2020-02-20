a2337fb2d7bf2e6b8f104ad7f82c842827fec8f7
skimedic <skimedic@gmail.com>
2020-02-19 22:07:35 -0500

    chapter 11

---
+++ TwoDayHandsOnLabFiles/3.1/Code/Completed/Lab11/SpyStore.Hol.Mvc/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using SpyStore.Hol.Mvc.Controllers.Base;
+using SpyStore.Hol.Mvc.Support;
+
+namespace SpyStore.Hol.Mvc.Controllers
+{
+    public class CartController : BaseController
+    {
+        private readonly SpyStoreServiceWrapper _serviceWrapper;
+
+        public CartController(SpyStoreServiceWrapper serviceWrapper, IConfiguration configuration) : base(configuration)
+        {
+            _serviceWrapper = serviceWrapper;
+        }
+    }
+}
---
+++ TwoDayHandsOnLabFiles/3.1/Code/Completed/Lab11/SpyStore.Hol.Mvc/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using SpyStore.Hol.Mvc.Controllers.Base;
+using SpyStore.Hol.Mvc.Support;
+
+namespace SpyStore.Hol.Mvc.Controllers
+{
+    public class ProductsController : BaseController
+    {
+        private readonly SpyStoreServiceWrapper _serviceWrapper;
+        public ProductsController(SpyStoreServiceWrapper serviceWrapper, IConfiguration configuration) : base(configuration)
+        {
+            _serviceWrapper = serviceWrapper;
+        }
+
+    }
+}
